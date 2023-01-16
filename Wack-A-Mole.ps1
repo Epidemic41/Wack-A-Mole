3d073c46f7174b3bf00e06e1908ced8f1c989666
Joseph H <12785608+josephH00@users.noreply.github.com>
2023-01-16 00:46:43 -0700

    Automatic detection of relevant services in categories (#2)
    * Added monitoring of multiple services
    
    * Fix invalid service status in check
    
    * Converted if-then service ladder to a multiple-entry list
    
    * Fix addition location for $monitoredServiceList
    
    * Fixed formatting
    
    * Add warning to user when selecting a group of services that don't exist on system
    
    * Added IIS & Filezilla FTP services to monitoring list
    
    * Consolidated HTTP & HTTPS monitoring listings
    
    * Added IIS HTTP/S services
    
    * Adjusted Formatting

--- Wack-A-Mole.ps1
+++ Wack-A-Mole.ps1
@@ -8,103 +8,150 @@ Write-Host "This tactic that is meant top the bleeding while defenders can ident
 #configure execution policy so script runs
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 
+$monitoredServiceList = [System.Collections.ArrayList]::new()
+
+class ServiceListEntry {
+    [ValidateNotNullOrEmpty()][string]$DisplayName      #eg. 'FTP' for FTP related services
+    [ValidateNotNullOrEmpty()][string[]]$Services       #eg. 'filezilla-server'
+}
+
+$servicesList = [ServiceListEntry[]]@(
+    [ServiceListEntry]@{
+        DisplayName = "FTP"
+        Services    = @(
+            "FTPSVC",           #IIS FTP Server
+            "filezilla-server"  #Filezilla FTP Server
+        )
+    },
+    [ServiceListEntry]@{
+        DisplayName = "HTTP/HTTPS"
+        Services    = @(
+            "HTTP",             #the HTTP 'service' is actually a driver needed for the Print Spooler, see https://superuser.com/questions/1059068/no-http-service-windows-10
+            
+            #IIS Services
+            "W3SVC",
+            "W3LOGSVC",
+            "WAS"
+        )
+    },
+    [ServiceListEntry]@{
+        DisplayName = "SMB"
+        Services    = @("SMB")
+    },
+    [ServiceListEntry]@{
+        DisplayName = "DNS"
+        Services    = @("DNS")
+    },
+    [ServiceListEntry]@{
+        DisplayName = "VNC"
+        Services    = @("VNC")
+    },
+    [ServiceListEntry]@{
+        DisplayName = "telnet"
+        Services    = @("Telnet")
+    }
+)
 
 #list of services to monitor, Last item is entering manual name
-Write-Host "Select a service to monitor"
-Write-Host "#1 FTP"
-Write-Host "#2 HTTP"
-Write-Host "#3 HTTPS"
-Write-Host "#4 SMB"
-Write-Host "#5 DNS"
-Write-Host "#6 VNC"
-Write-Host "#7 SSH"
-Write-Host "#8 telnet"
-Write-Host "#9 Custom"
+Write-Host "Select which services to monitor (separated by commas)"
+
+$servicesList | ForEach-Object { $index = 1 } { Write-Host "#$($index)" $_.DisplayName; $index++ }  #Print display name of all registered services 
+
+Write-Host "#$($servicesList.Length + 1) Custom" #Custom option
 
 #prompt user for number
-$optionNumber = read-host "Select a number from above list: "
+$optionNumbers = read-host "Select a number from above list: "
 
-#Nine if statements to determine the $serviceName Value based on user input
-if ($optionNumber -eq 1){
-    $serviceName = "FTP"
-}
-if ($optionNumber -eq 2){
-    $serviceName = "HTTP"
-}
-if ($optionNumber -eq 3){
-    $serviceName = "HTTPS"
-}
-if ($optionNumber -eq 4){
-    $serviceName = "SMB"
-}
-if ($optionNumber -eq 5){
-    $serviceName = "DNS"
-}
-if ($optionNumber -eq 6){
-    $serviceName = "VNC"
-}
-if ($optionNumber -eq 7){
-    $serviceName = "SSH"
-}
-if ($optionNumber -eq 8){
-    $serviceName = "Telnet"
-}
-if ($optionNumber -eq 9){
-    #This last option promps user for custom service not listed above
-    $serviceName = Read-Host "Enter Service Name (not display name), confirm after setup that script is running correctly"
-}
+foreach ($option in $optionNumbers.Split(",")) {
+    $option = $option.trim() #remove whitespace left over from Split()
 
+    if ($option -le $servicesList.Length) {
+
+        #true if any of the group of services under the display name exist on the system 
+        $displayNameHasExistentServices = $false;
+
+        #get all services listed under the display name
+        $servicesList[[int]$option - 1].Services | ForEach-Object {
+
+            #assigns the service under the display name to the watched services list if it exists on the system 
+            # (eg. When selecting the 'FTP' option, fillezilla-server will be added to the list but FTPSrv won't if the IIS FTP Server feature isn't installed); ignores silently if service doesn't exist
+            $serviceExists = Get-Service -Name $_ -ErrorAction SilentlyContinue
+            if ($null -ne $serviceExists) {
+                $monitoredServiceList.Add($_) #add previously existing service name to monitoring list
+
+                $displayNameHasExistentServices = $true
+            }
+        }
+
+        #warn the user when attempting to add a set of services that don't exist
+        if ($displayNameHasExistentServices -eq $false) {
+            $serviceListEntry = $servicesList[[int]$option - 1]
+            $displayName = $serviceListEntry.DisplayName
+
+            Write-Warning "No services under group '$displayName' are present on this system! ($($serviceListEntry.Services -join ", "))"
+            Write-Warning "Check selection(s) from above are entered correctly."
+        }
+    }
+    else {
+        #This last option promps user for custom service not listed above
+        $serviceName = Read-Host "Enter Service Name (not display name), confirm after setup that script is running correctly"
+        $monitoredServiceList.Add($serviceName) #add custom service name to monitoring list
+    }
+    
+}
 #formatting
 Write-host "---------------------------------------------------------------------------------------------------------"
 
 #while true loop
 $wack = "true"
-while ($wack -eq "true"){
+while ($wack -eq "true") {
+    foreach ($serviceName in $monitoredServiceList) {
 
-    #check if service is up, 
-    if ((Get-Service $serviceName).Status -eq "Running"){
-
-        #display artifacts
-        Write-Host $serviceName" is Running"
-        (Get-Date).ToString('G')
-        Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
-
-
-    }
-
-    #if service is Not enabled
-    if ((Get-Service $serviceName).Status -ne "Running"){
-
-        #display artifacts
-        write-host $serviceName" is not running. Attempting to start service."
-        (Get-Date).ToString('G')
-        Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
-
-        #try to start service
-        Start-Service -Name $serviceName
-
-        #if service is now enabled
-        if ((Get-Service $serviceName).Status -eq "Enabled"){
+        #check if service is up, 
+        if ((Get-Service $serviceName).Status -eq "Running") {
 
             #display artifacts
-            write-host $serviceName" is now running."
+            Write-Host $serviceName" is Running"
             (Get-Date).ToString('G')
             Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
+
+
         }
-        
-        #if service is still not running after the above attempt to get it running
-        if ((Get-Service $serviceName).Status -ne "Running"){
+
+        #if service is Not enabled
+        if ((Get-Service $serviceName).Status -ne "Running") {
 
             #display artifacts
-            write-host "Unable to start "$serviceName". Investigate before the mole wacks you."
+            write-host $serviceName" is not running. Attempting to start service."
             (Get-Date).ToString('G')
-
             Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
 
-            #stops while loop so Incident responder can investigate
-            $wack = "false"
+            #try to start service
+            Start-Service -Name $serviceName
 
-            #wacked ascii art here
+            #if service is now starting or running
+            if ((Get-Service $serviceName).Status -Match "Running|StartPending") { 
+
+                #display artifacts
+                write-host $serviceName" is now running."
+                (Get-Date).ToString('G')
+                Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
+            }
+        
+            #if service is still not running after the above attempt to get it running
+            if ((Get-Service $serviceName).Status -ne "Running") {
+
+                #display artifacts
+                write-host "Unable to start "$serviceName". Investigate before the mole wacks you."
+                (Get-Date).ToString('G')
+
+                Get-Service $serviceName | Format-Table serviceName, displayName, startType, status
+
+                #stops while loop so Incident responder can investigate
+                $wack = "false"
+
+                #wacked ascii art here
+            }
         }
     }
 
@@ -114,11 +161,14 @@ while ($wack -eq "true"){
 
 
     #potental new features
-        #write output to log
-        #user can pause loop to look at output
-            #check for input, timeout
-
+    #write output to log
+    #user can pause loop to look at output
+    #check for input, timeout
+    
 } # end of while loop
 
 #reference
 #https://social.technet.microsoft.com/Forums/windowsserver/en-US/79bf9de7-1c17-45c0-a02b-7558af89807a/powershell-script-to-check-service-status
+#IIS Services 
+# https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/jj635851(v=ws.11), 
+# https://docs.oracle.com/en/industries/health-sciences/inform/cognos1117-install/index.html?toc.htm?226411.htm)
